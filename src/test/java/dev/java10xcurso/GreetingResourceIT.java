36b2fe0a52f3fcfa06c642332b0f4af77132f1e8
quarkusio <no-reply@quarkus.io>
2026-04-08 11:36:41 +0000

    Initial commit

---
+++ src/test/java/dev/java10xcurso/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package dev.java10xcurso;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
